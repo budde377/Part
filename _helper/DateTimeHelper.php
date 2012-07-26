28703db41af7fbc553eb46a2f703ca9f0f5a3e0b
budde <christi@n-bud.de>
2012-07-26 17:21:37 +0200

    Added dateTimeHelper

---
+++ _helper/DateTimeHelper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: budde
+ * Date: 26/07/12
+ * Time: 17:15
+ */
+class DateTimeHelper
+{
+
+    public static function monthNumberToName($number){
+        $monthArray = array('januar','februar','marts','april','maj','juni','juli','august','september','november','december');
+        return $monthArray[$number%12];
+    }
+
+    public static function dayNumberToName($number){
+        $monthArray = array('mandag','tirsdag','onsdag','torsdag','fredag','lørdag','søndag');
+        return $monthArray[$number%7];
+    }
+
+    
+
+}
