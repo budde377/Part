ce64d4f47c976670600ae13fb45643eb6c4da95a
budde <christi@n-bud.de>
2012-09-11 23:26:21 +0200

    Renamed MailTrait to ValidationTrait for future purpose of file

--- _trait/ValidationTrait.php
+++ _trait/ValidationTrait.php
@@ -5,7 +5,7 @@
  * Date: 11/09/12
  * Time: 21:13
  */
-trait MailTrait
+trait ValidationTrait
 {
     private function validMail($mail){
         return @preg_match('/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i',$mail) == 1;
