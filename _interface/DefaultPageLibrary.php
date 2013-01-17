f427aed451f86ed84298dbf46de4d465f5006f81
budde <christi@n-bud.de>
2013-01-17 22:41:12 +0100

    Added methods to interface

--- _interface/DefaultPageLibrary.php
+++ _interface/DefaultPageLibrary.php
@@ -5,7 +5,20 @@
  * Date: 17/01/13
  * Time: 22:36
  */
-interface DefaultPageLibrary
+interface DefaultPageLibrary extends Iterator
 {
+    /**
+     * Will list all default pages, as defined in given config.
+     * @return array An array containing instances of Page representing the Default pages
+     */
+    public function listPages();
+
+    /**
+     * Will return a default page given an ID
+     * @param string $id The id
+     * @return Page | null Instance matching the ID or NULL on no such page
+     */
+    public function getPage($id);
+
 
 }
