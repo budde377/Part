b6ec74b792960c9bb7a42056f87f9bd912235690
Christian Budde Christensen <budde377@cs.au.dk>
2014-03-12 22:28:58 +0100

    Added more errorcodes

--- lib/interfaces/JSONResponse.php
+++ lib/interfaces/JSONResponse.php
@@ -32,6 +32,11 @@ interface JSONResponse extends JSONElement
     const ERROR_CODE_INVALID_FILE = 19;
     const ERROR_CODE_FILE_NOT_FOUND = 20;
     const ERROR_CODE_COULD_NOT_CREATE_FILE = 21;
+    const ERROR_CODE_INVALID_NAME = 22;
+    const ERROR_CODE_INVALID_SUBJECT = 23;
+    const ERROR_CODE_INVALID_MESSAGE = 24;
+    const ERROR_CODE_INVALID_INPUT = 25;
+
     /**
      * @return String
      */
