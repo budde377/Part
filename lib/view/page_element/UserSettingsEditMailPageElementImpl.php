1822f62fed9b5aeb03736b7518900f4ee7ca0c3b
Christian Budde Christensen <budde377@cs.au.dk>
2014-10-12 17:07:26 -0400

    Further development on the usersettings mail admin panel UI.

--- lib/view/page_element/UserSettingsEditMailPageElementImpl.php
+++ lib/view/page_element/UserSettingsEditMailPageElementImpl.php
@@ -9,7 +9,8 @@ namespace ChristianBudde\cbweb\view\page_element;
  * Time: 9:29 AM
  */
 
-class UserSettingsEditMailPageElementImpl extends PageElementImpl{
+class UserSettingsEditMailPageElementImpl extends PageElementImpl
+{
     public function generateContent()
     {
         $out = "
@@ -18,8 +19,24 @@ class UserSettingsEditMailPageElementImpl extends PageElementImpl{
         <li>christianbud.de</li>
         <li>christian-budde.dk</li>
         <li>christianbudde.dk</li>
-            <li class='add'></li>
         </ul>
+        <form id='UserSettingsEditMailAddDomainForm' class=' mail_form'>
+            <input type='checkbox' class='enable_state_checkbox' id='UserSettingsEditMailAddDomainFormCheckbox'/>
+            <label for='UserSettingsEditMailAddDomainFormCheckbox' class='no_select'></label>
+            <div class='mail_form_container'>
+            <label>
+                Domæne
+                <input type='text' />
+            </label>
+            <label>
+                Super-kodeord
+                <input type='password' />
+            </label>
+            <div class='submit'>
+            <input type='submit' value='Opret Alias'/>
+            </div>
+            </div>
+        </form>
         <h3>Domæne alias</h3>
         <ul class='floating_list points_to'>
             <li>
@@ -33,9 +50,31 @@ class UserSettingsEditMailPageElementImpl extends PageElementImpl{
                     christianbudde.dk
                 </div>
             </li>
-            <li class='add'></li>
         </ul>
-
+        <form id=\"UserSettingsEditMailAddDomainAliasForm\" class=' mail_form'>
+            <input type='checkbox' class='enable_state_checkbox' id='UserSettingsEditMailAddDomainAliasFormCheckbox'/>
+            <label for='UserSettingsEditMailAddDomainAliasFormCheckbox' class='no_select'></label>
+            <div class='mail_form_container'>
+            <label>
+                Domæne
+                <select>
+                    <option>christianbudde.dk</option>
+                    <option>christian-budde.dk</option>
+                </select>
+            </label>
+            <label>
+                Peger på
+                <select>
+                    <option>christianbudde.dk</option>
+                    <option>christian-budde.dk</option>
+                    <option>christianbud.de</option>
+                </select>
+            </label>
+            <div class='submit'>
+            <input type='submit' value='Opret Alias'/>
+            </div>
+            </div>
+        </form>
         <h3>Adresser</h3>
         <ul class='floating_list'>
             <li>test@christian-budde.dk</li>
@@ -46,7 +85,6 @@ class UserSettingsEditMailPageElementImpl extends PageElementImpl{
             <li>test5@christian-budde.dk</li>
             <li>test6@christian-budde.dk</li>
             <li>test7@christian-budde.dk</li>
-            <li class='add'></li>
         </ul>
         <ul class='floating_list'>
             <li>bob@christianbudde.dk</li>
@@ -57,9 +95,62 @@ class UserSettingsEditMailPageElementImpl extends PageElementImpl{
             <li>bob5@christianbudde.dk</li>
             <li>bob6@christianbudde.dk</li>
             <li>bob7@christianbudde.dk</li>
-            <li class='add'></li>
         </ul>
+        <form id='UserSettingsEditMailAddAddressForm' class='mail_form'>
 
+            <input type='checkbox' class='enable_state_checkbox' id='UserSettingsEditMailAddAddressFormCheckbox'/>
+            <label for='UserSettingsEditMailAddAddressFormCheckbox' class='no_select'></label>
+            <div class='mail_form_container'>
+            <label>
+                Navn
+                <input type='text'>
+            </label>
+            <span class='at'>@</span>
+            <label>
+                Domæne
+                <select>
+                    <option>christianbudde.dk</option>
+                    <option>christian-budde.dk</option>
+                    <option>christianbud.de</option>
+                </select>
+            </label>
+            <label>
+                Vælg brugere
+            </label>
+            <ul class='owner_check_list'>
+                <li>
+                    <input type='checkbox' id='someUniqueId'/>
+                    <label for='someUniqueId'>
+                        budde377
+                    </label>
+                </li>
+                <li>
+                    <input type='checkbox' id='someUniqueId2'/>
+                    <label for='someUniqueId2'>
+                        bent
+                    </label>
+                </li>
+            </ul>
+            <label class='long_input'>
+                Vidersend til (komma separeret liste)
+                <input type='text' />
+            </label>
+            <input type='checkbox' id='UserSettingsEditMailAddAddressAddMailboxCheckbox'/>
+            <label for='UserSettingsEditMailAddAddressAddMailboxCheckbox' class='long_input'>
+                Opret mailbox:
+            </label>
+            <label>
+            Mailbox kodeord
+            <input type='password'>
+            </label>
+            <div class='submit'>
+                <input type='submit' value='Opret adresse' />
+
+            </div>
+
+            </div>
+
+        </form>
         ";
 
 
