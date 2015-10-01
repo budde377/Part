6f9b3f935b0f9a1ead1006cb98b5c95081d1aa04
Christian Budde Christensen <budde377@gmail.com>
2015-10-01 17:50:56 +0200

    Made SortedChildrenGenerator compare on pairs

--- dart/part/lib/src/elements_children_generator.dart
+++ dart/part/lib/src/elements_children_generator.dart
@@ -9,8 +9,7 @@ class ElementChildrenGenerator<K, V extends Element> extends core.Generator<K, V
     return list;
   }
 
-  factory ElementChildrenGenerator(V generator(K), Element elm, K selector(V, Element elm))
-  => new ElementChildrenGenerator._internal(
+  factory ElementChildrenGenerator(V generator(K), Element elm, K selector(V, Element elm)) => new ElementChildrenGenerator._internal(
       generator,
       elm,
       selector,
@@ -38,25 +37,36 @@ class ElementChildrenGenerator<K, V extends Element> extends core.Generator<K, V
 
 class SortedElementChildrenGenerator<K, V extends Element> extends ElementChildrenGenerator<K, V> {
 
-  factory SortedElementChildrenGenerator.fromDataset(String entry, V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator(
+  factory SortedElementChildrenGenerator.fromDataset(String entry, V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator.fromValue(
           (Element e1, Element e2) => e1.dataset[entry].compareTo(e2.dataset[entry]),
       generator, element, selector);
 
-  factory SortedElementChildrenGenerator.fromText(V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator(
+  factory SortedElementChildrenGenerator.fromText(V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator.fromValue(
           (Element e1, Element e2) => e1.text.compareTo(e2.text),
       generator, element, selector);
 
-  SortedElementChildrenGenerator(int compare(V v1, V v2), V generator(K), Element element, K selector(V, Element elm)):
+  factory SortedElementChildrenGenerator.fromKey(int compare(K k1, K k2), V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator(
+          (core.Pair<K, V> p1, core.Pair<K, V> p2) => compare(p1.k, p1.k),
+      generator, element, selector);
+
+  factory SortedElementChildrenGenerator.fromValue(int compare(V k1, V k2), V generator(K), Element element, K selector(V, Element elm)) => new SortedElementChildrenGenerator(
+          (core.Pair<K, V> p1, core.Pair<K, V> p2) => compare(p1.v, p1.v),
+      generator, element, selector);
+
+  SortedElementChildrenGenerator(int compare(core.Pair<K, V> p1, core.Pair<K, V> p2), V generator(K), Element element, K selector(V, Element elm)):
   super._internal(generator, element, selector,
-  _updateOrder(element, compare),
+  _updateOrder(element, compare, selector),
       (core.Pair<K, V> pair) => pair.v.remove()) {
-    onUpdate.listen(_updateOrder(element, compare));
+
+    onUpdate.listen(_updateOrder(element, compare, selector));
   }
 
-  static _updateOrder(element, compare) {
+  static _updateOrder(Element element, int compare(core.Pair<dynamic, Element> p1, core.Pair<dynamic, Element> p2), dynamic selector(Element, Element)) {
     return (core.Pair pair) {
       pair.v.remove();
-      var child = element.children.firstWhere((Element child) => compare(child, pair.v) > 0, orElse: () => null);
+      var child = element.children.firstWhere(
+              (Element child) => compare(new core.Pair(selector(child, element), child), pair) > 0,
+              orElse: () => null);
       if (child != null) {
         child.insertAdjacentElement('beforeBegin', pair.v);
       } else {
